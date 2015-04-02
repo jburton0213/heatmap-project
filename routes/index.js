1c82fc1531f327afcc86b293244cf043fc10ec97
unknown <jburton@WSHNCDAS01L.tinsight.corp>
2015-04-02 00:32:45 -0400

    adding IP tracker

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var pg = require('pg');
+//var dbURL = "postgres://leyorvqudqwkay:47OUnM4sVxGeiAn-MJIr7-ErRp@ec2-50-19-249-214.compute-1.amazonaws.com:5432/d7bp9obaffs826"
+var dbURL = process.env.DATABASE_URL;
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var ipAddress = req.connection.remoteAddress;
+  var d = new Date();
+  var query = "insert into ipaddresses values(\'" + ipAddress + "\', \'" + d + "\')" 
+  pg.connect(dbURL, function(err, client) {
+  	client.query(query, function(err, result) {
+  		done();
+  		if(err) throw err;
+  		client.end();
+  	})
+  })
   res.render('index', { title: 'Express' });
 });
 
