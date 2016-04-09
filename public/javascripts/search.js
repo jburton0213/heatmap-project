1aec907ae1a6b91a139d76b8d91d519a67de03b9
Joshua Burton <jburton@centuryfurniture.com>
2016-04-09 19:34:19 -0400

    Adding pagation to the /search page.

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,9 +1,18 @@
 ﻿$(document).ready(function () {
     $(searchNav).addClass("active");
-    $(submitbutton).click(function () {
-        //$(tableDiv).html("<img src=\"/images/kat-loading.gif\" class=\"center-block\">");
+    $(submitbutton).click(callAPIandUpdateTable)
+})
+
+$('#searchform').keypress(function (e) {
+    var key = e.which;
+    if(key == 13){ // the enter key code
+        // callAPIandUpdateTable()
+        $(submitbutton).click(); 
+    };
+}); 
+
+function callAPIandUpdateTable(pageNumber) {
         $(tableDiv).html("<img src='/images/loading-gears-animation-3.gif' class='center-block'>");
-        
         var address = encodeURIComponent($('#address').val());
         var zip = encodeURIComponent($('#zip').val());
         var name = encodeURIComponent($('#name').val());
@@ -13,8 +22,7 @@
         var bedrooms = encodeURIComponent($('#bedrooms').val());
         var bathrooms = encodeURIComponent($('#bathrooms').val());
         
-        var url = "/query/?"
-        var jsonText;
+        var url = "/query/?";
         
         if (address) url = url + "address=" + address + "&";
         if (zip) url = url + "zip=" + zip + "&";
@@ -24,6 +32,7 @@
         if (city) url = url + "city=" + city + "&";
         if (bedrooms) url = url + "bedrooms=" + bedrooms + "&";
         if (bathrooms) url = url + "bathrooms=" + bathrooms + "&";
+        if (typeof pageNumber === "number") url = url + "page=" + pageNumber;
         
         var request = $.ajax({
             method: "GET",
@@ -31,7 +40,10 @@
             dataType: "json"
         });
 
-        request.done(function (data) {            
+        request.done(function (data) {
+            
+            // $(tableDiv).html(data.items[1].gender);
+                        
             var tableHeader = "";
             tableHeader = tableHeader + "<table class='table'>";
             tableHeader = tableHeader + "<thead>";
@@ -54,28 +66,64 @@
             tableHeader = tableHeader + "<tbody>";
             var tableData = "";
             
-            for (var i = 0; i < data.length; i++) {
-                var dataitem = "<tr><td>" + data[i].name + "</td><td>" + data[i].gender + "</td><td>" + data[i].age + "</td><td>" + data[i].race + "</td><td>" + data[i].party + "</td><td>" + data[i].address + "</td><td>" + data[i].city + "</td><td>" + data[i].zip + "</td><td>" + data[i].taxAssessment + "</td><td>" + data[i].finishedSqFt + "</td><td>" + data[i].yearBuilt + "</td><td>" + data[i].lastSoldDate + "</td><td>" + data[i].lastSoldPrice + "</td></tr>";
+            for (var i = 0; i < data.items.length; i++) {
+                var dataitem = "<tr><td>" + data.items[i].name + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].gender + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].age + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].race + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].party + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].address + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].city + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].zip + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].taxAssessment + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].finishedSqFt + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].yearBuilt + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].lastSoldDate + "</td>";
+                dataitem = dataitem + "<td>" + data.items[i].lastSoldPrice + "</td></tr>";
                 tableData = tableData + dataitem;
             };
             
-            var footer = "</tbody></table>";
-            var htmltosend = tableHeader + tableData + footer;
+            var tablefooter = "</tbody></table>";
+            
+            var nav = "     <div class=\"pager\">"
+                nav = nav + "   <ul class=\"pagination bootpag\">"
+                nav = nav + "       <li>"
+                nav = nav + "           <a href=\"#\" aria-label=\"Previous\">"
+                nav = nav + "           <span aria-hidden=\"true\">&laquo;</span>"
+                nav = nav + "           </a>"
+                nav = nav + "       </li>"
+                nav = nav + "       <li><a href=\"#\">1</a></li>"
+                nav = nav + "       <li><a href=\"#\">2</a></li>"
+                nav = nav + "       <li><a href=\"#\">3</a></li>"
+                nav = nav + "       <li><a href=\"#\">4</a></li>"
+                nav = nav + "       <li><a href=\"#\">5</a></li>"
+                nav = nav + "       <li>"
+                nav = nav + "           <a href=\"#\" aria-label=\"Next\">"
+                nav = nav + "           <span aria-hidden=\"true\">&raquo;</span>"
+                nav = nav + "           </a>"
+                nav = nav + "       </li>"
+                nav = nav + "    </ul>"
+                nav = nav + "</div>"
+            
+            var htmltosend = tableHeader + tableData + tablefooter + nav;
             $(tableDiv).html(htmltosend);
-
+            
+            var pageCount = 1
+            
+            if (data.resultsCount > 10){
+                pageCount = data.resultsCount / 10
+            }
+            
+            $('.pager').bootpag({
+                total : pageCount,
+                page : data.page,
+                maxVisible : 10
+                }).on("page", function(event, num){
+                    callAPIandUpdateTable(num)
+                })
         })
 
         request.fail(function (jqXHR, textStatus) { 
             $(tableDiv).html("Something unexpected happened.  This could help: " + textStatus);
         })
-    })
-})
-
-$('#searchform').keypress(function (e) {
- var key = e.which;
- if(key == 13)  // the enter key code
-  {
-    $('input[name = submitbutton]').click();
-    return false;  
-  }
-}); 
+    }
--- routes/query.js
+++ routes/query.js
@@ -5,6 +5,9 @@ var mongodb = require('mongodb')
 router.get('/', function (req, res, next) {
     
 	var uri = "mongodb://readonly:gjvlejh8567jcvneu8@ds059821.mongolab.com:59821/heroku_app33948089"
+    var skip = 0
+    var page = 1;
+    var objectTOSend = new Object()
     
     if (req.query.name) req.query.name = new RegExp('.*' + req.query.name.toUpperCase().replace(" ",".*").replace(",",".*") + '.*')
     if (req.query.bedrooms) req.query.bedrooms = parseInt(req.query.bedrooms);
@@ -14,21 +17,24 @@ router.get('/', function (req, res, next) {
     if (req.query.party) req.query.party = new RegExp('.*' + req.query.party.toUpperCase() + '.*');
     if (req.query.address) req.query.address = new RegExp('.*' + req.query.address.toUpperCase() + '.*');
     if (req.query.gender) req.query.gender = new RegExp('.*' + req.query.gender.toUpperCase() + '.*');
-    
-    // if (req.query.bedrooms) req.query.bedrooms = parseInt(req.query.bedrooms);
-    // if (req.query.bathrooms) req.query.bathrooms = parseInt(req.query.bathrooms);
-    // if (req.query.zip) req.query.zip = parseInt(req.query.zip);
-    // if (req.query.city) req.query.city = req.query.city.toUpperCase();
-    // if (req.query.party) req.query.party = req.query.party.toUpperCase();
-    // if (req.query.address) req.query.address = req.query.address.toUpperCase();
-    // if (req.query.gender) req.query.gender = req.query.gender.toUpperCase();
+    if (req.query.page) { 
+        skip = (req.query.page - 1) * 10;
+        page = req.query.page;
+        req.query.page = null;
+    }
 
     mongodb.MongoClient.connect(uri, function (err, db) {
         if (err) throw err;
         var voters = db.collection('voters');
-        voters.find(req.query, {limit: 10, fields: { _id: 0 }}).toArray(function (err, items) {
-            if (err) throw err;
-            res.send(items);
+        voters.find(req.query, {fields: {processed: 1 }}).count(function (err, count) {
+            if (err) console.log(err)
+            objectTOSend.resultsCount = count
+                voters.find(req.query, {limit: 10, skip: skip, fields: { _id: 0 }}).toArray(function (err, items) {
+                    if (err) throw err;  
+                    objectTOSend.items = items;
+                    objectTOSend.page = page;                  
+                    res.send(JSON.stringify(objectTOSend));
+            }) 
         })
     })
 })
