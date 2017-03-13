cd9537b08912a342aba47a240133bb5b782a8234
Joshua Burton <joshjb213@gmail.com>
2017-03-13 09:23:02 -0400

    Adding connection strings

---
+++ connectionstrings.js
@@ -0,0 +1,3 @@
+exports.mongoreadonly = 
+exports.mongoinsert = "mongodb://insert:jdur784khi9erj@ds059821.mlab.com:59821/heroku_app33948089";
+exports.mongoreadonly = "mongodb://readonly:gjvlejh8567jcvneu8@ds059821.mongolab.com:59821/heroku_app33948089"
